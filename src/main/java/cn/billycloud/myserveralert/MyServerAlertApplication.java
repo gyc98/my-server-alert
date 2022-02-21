db566ce749d5e1669f79c61f74f16c18a39269ea
Yuanchen Gao <gyc98@outlook.com>
2022-02-21 22:13:35 +0800

    First Commit

---
+++ src/main/java/cn/billycloud/myserveralert/MyServerAlertApplication.java
@@ -0,0 +1,13 @@
+package cn.billycloud.myserveralert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServerAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyServerAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/billycloud/myserveralert/MyServerAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.billycloud.myserveralert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServerAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
