f41a48bc8b6aabcc6e188750e5cdef76b1982b6b
Yuanchen Gao <gyc98@outlook.com>
2022-02-22 21:03:40 +0800

    处理日志相关

--- src/main/java/cn/billycloud/myserveralert/MyServerAlertApplication.java
+++ src/main/java/cn/billycloud/myserveralert/MyServerAlertApplication.java
@@ -1,13 +1,16 @@
 package cn.billycloud.myserveralert;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MyServerAlertApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyServerAlertApplication.class, args);
+
+		log.info("hello world!");
 	}
 
 }
