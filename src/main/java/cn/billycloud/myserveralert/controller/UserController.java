cfd2cc0c0e40fcc74b438cbbde38384741f2f69c
Yuanchen Gao <gyc98@outlook.com>
2022-02-25 15:03:31 +0800

    添加注册用户Controller

---
+++ src/main/java/cn/billycloud/myserveralert/controller/UserController.java
@@ -0,0 +1,34 @@
+package cn.billycloud.myserveralert.controller;
+
+import cn.billycloud.myserveralert.service.UserService;
+import cn.billycloud.myserveralert.util.Result;
+import cn.billycloud.myserveralert.util.ResultCode;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/register")
+    @ResponseBody
+    public Result register(@RequestBody JSONObject jsonObject){
+        String userName = jsonObject.getString("userName");
+        String passwordSet = jsonObject.getString("passwordSet");
+        log.info("收到注册请求 用户名：" + userName + " 密码：" + passwordSet);
+        Result result = userService.addNewUser(userName, passwordSet);
+        return result;
+    }
+}
