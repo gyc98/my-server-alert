505dd433a5d9e0ec280f2c142f1b9acd3d5e6991
Yuanchen Gao <gyc98@outlook.com>
2022-03-02 19:45:19 +0800

    maven打包

--- src/main/java/cn/billycloud/myserveralert/MyServerAlertApplication.java
+++ src/main/java/cn/billycloud/myserveralert/MyServerAlertApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.redis.core.RedisTemplate;
